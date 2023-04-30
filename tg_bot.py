743a23839de5ab4b8021b89c1e3dd766858a4703
EgoisTa <97385729+EgoisTa-Git@users.noreply.github.com>
2023-04-30 22:01:24 +0300

    Setup TG bot

--- tg_bot.py
+++ tg_bot.py
@@ -1,44 +1,111 @@
-import logging
+import re
 
 from environs import Env
-from telegram import Update
+import redis
+from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import (
     Updater,
     CommandHandler,
     MessageHandler,
     Filters,
     CallbackContext,
+    ConversationHandler,
 )
 
-# logging.basicConfig(
-#     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-# )
-# logger = logging.getLogger(__name__)
+NEW_QUESTION, ANSWER = range(2)
 
 
 def start(update: Update, context: CallbackContext):
+    quiz_keyboard = [['Новый вопрос', 'Сдаться'], ['Мой счет', ]]
+    reply_markup = ReplyKeyboardMarkup(quiz_keyboard)
     update.message.reply_text(
-        'Здравствуйте',
+        text='Привет! Я бот для викторин!',
+        reply_markup=reply_markup
     )
+    return NEW_QUESTION
 
 
-def echo(update: Update, context: CallbackContext):
-    update.message.reply_text(update.message.text)
+def handle_new_question_request(update: Update, context: CallbackContext):
+    question = questions_db.randomkey()
+    update.message.reply_text(question, reply_markup=ReplyKeyboardRemove())
+    users_db.set(update.message.chat_id, question)
+    return ANSWER
 
 
-def main(tg_token):
-    updater = Updater(tg_token)
-    dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler("start", start))
+def handle_solution_attempt(update: Update, context: CallbackContext):
+    question = users_db.get(update.message.chat_id)
+    answer = questions_db.get(question).lstrip('Ответ: ').rstrip('.').strip()
+    answer = answer.replace('"', '')
+    correct_answer = re.sub(r'[\(\[].*?[\)\]]', '', answer).lower()
+    if correct_answer == '':
+        correct_answer = answer.strip('[]()').lower()
+    if update.message.text.strip('"[]()').lower() == correct_answer:
+        update.message.reply_text('Правильно! Поздравляю!')
+        handle_new_question_request(update, context)
+    else:
+        update.message.reply_text('Неправильно… Попробуешь ещё раз?')
+        return ANSWER
 
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 
-    updater.start_polling()
-    updater.idle()
+def handle_give_up_request(update: Update, context: CallbackContext):
+    question = users_db.get(update.message.chat_id)
+    answer = questions_db.get(question)
+    update.message.reply_text(answer)
+    update.message.reply_text('Не расстраивайся! В следующий раз обязательно получится!')
+    handle_new_question_request(update, context)
+
+
+def handle_my_score_request(update: Update, context: CallbackContext):
+    update.message.reply_text('Эта функция будет доступна позднее. Надеемся на Ваше понимание.')
+
+
+def cancel(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        'До новых встреч!',
+        reply_markup=ReplyKeyboardRemove(),
+    )
+    return ConversationHandler.END
 
 
 if __name__ == '__main__':
     env = Env()
     env.read_env()
     tg_bot_api_key = env('TG_BOT_APIKEY')
-    main(tg_bot_api_key)
+    redis_db_pass = env('REDIS_DB_PASS')
+    questions_db = redis.Redis(
+        host='localhost',
+        port=6379,
+        password=redis_db_pass,
+        decode_responses=True,
+        db=0,
+    )
+    users_db = redis.Redis(
+        host='localhost',
+        port=6379,
+        password=redis_db_pass,
+        decode_responses=True,
+        db=1,
+    )
+
+    updater = Updater(tg_bot_api_key)
+    dispatcher = updater.dispatcher
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            NEW_QUESTION: [
+                MessageHandler(Filters.regex(r'^Новый вопрос'), handle_new_question_request),
+                MessageHandler(Filters.regex(r'^Мой счет'), handle_my_score_request),
+            ],
+            ANSWER: [
+                MessageHandler(Filters.regex(r'^Сдаться'), handle_give_up_request),
+                MessageHandler(Filters.regex(r'^Мой счет'), handle_my_score_request),
+                MessageHandler(Filters.text, handle_solution_attempt),
+            ],
+        },
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
+    dispatcher.add_handler(conv_handler)
+
+    updater.start_polling()
+    updater.idle()
--- upload_questions.py
+++ upload_questions.py
@@ -1,4 +1,8 @@
 from pathlib import Path
+import re
+
+from environs import Env
+import redis
 
 
 def normalize_text(phrase):
@@ -16,17 +20,27 @@ def get_pairs(lines):
     questions = []
     answers = []
     for line in lines:
-        if 'Вопрос' in line:
+        if re.match(r'Вопрос \d+:', line):
             questions.append(normalize_text(line))
-        elif 'Ответ' in line:
+        elif 'Ответ:' in line:
             answers.append(normalize_text(line))
     return dict(zip(questions, answers))
 
 
 if __name__ == '__main__':
     file_paths = get_file_paths('questions')
-    pairs = {}
+    env = Env()
+    env.read_env()
+    redis_db_pass = env('REDIS_DB_PASS')
+    questions_db = redis.Redis(
+        host='localhost',
+        port=6379,
+        password=redis_db_pass,
+        decode_responses=True,
+        db=0,
+    )
     for path in file_paths:
         with open(path, encoding='KOI8-R') as file:
-            text = file.read().split('\n\n')
-        pairs.update(get_pairs(text))
+            texts = file.read().split('\n\n')
+        for question, answer in get_pairs(texts).items():
+            questions_db.set(question, answer)
